87947db0105c4507d6afb6295c82fbb6324f3cdb
Hammad-Zahoor <141130337+Hammad-Zahoor@users.noreply.github.com>
2024-03-09 23:36:26 +0500

    Create core_concepts.js

---
+++ core_concepts.js
@@ -0,0 +1,139 @@
+// Variables   case sensitive
+// console.log("Variables")
+// var a = 70    // old method
+// console.log(a)
+// a = "Harry"
+// {
+//     let a = "Hammad"
+//     console.log(a)   // block level,  cannot be re-declare
+// }
+// console.log(a)
+// const d = 9
+// console.log(d)
+
+// // Primitives and Objects
+// console.log("Primitives")  // 7 data-types
+// let a = null
+// let b = 345
+// let c = "Hammad"
+// let d = undefined
+// let e = Symbol("Hello")
+// let f = BigInt("1232333")
+// let g = true
+// console.log(a,b,c,d,e,f,g)
+// console.log(typeof(d))
+
+// console.log("Objects")  // non-preemptive data-types   key-value mapping
+// const item = {
+//     a : "Harry",
+//     b : 2
+// }
+// console.log(item["b"])
+// item["a"] = null
+// console.log(item.a)
+
+// // Operators and Expressions
+// console.log("Operators")
+// let a = 2
+// let b = 3
+// console.log(a**b)
+// console.log(a%b)
+// console.log(a++)  // it will increase value but will show you in the next output
+// console.log(a)
+// console.log(++a)
+// let k = 2
+// k **= 2
+// console.log(k)
+
+// let val1 = 2
+// let val2 ="2"
+// console.log(val1 == val2)
+// console.log(val1 === val2)
+// console.log(val1 > 2 && val2 < 3)
+
+// conditional expression
+//let a = prompt("What is your age?")  //prompt and alert are browsing expression
+// let a = "233"
+// a = Number.parseInt(a)
+// console.log(typeof(a))
+
+// if(a > 100){
+//     console.log("TRUE")
+// }
+// else if(a > 200){
+//     console.log("Mostly TRUE")
+// }
+
+// const expression = "apple"
+// switch(expression){
+//     case "orange":
+//         console.log("orange")
+//         break;
+//     case "apple":
+//         console.log("apple")
+//         break;
+//     default:
+//         console.log("Default")        
+// }
+
+// // Ternary operator
+// let age = 20
+// console.log("You ",age<18?"cannot drive":"drive")
+
+// Loops 
+// for(let i=0;i<4;i++)
+// {
+//     console.log(i)
+// }
+// let item = {
+//     name:"Hammad",
+//     age:23
+// }
+// for(a in item){
+//     console.log(a , item[a])
+// }
+// for(a of "Haris"){
+//     console.log(a)
+// }
+
+// let i = 2
+// while(i<5){
+//     console.log(i)
+//     i++
+// }
+
+// let j = 2
+// do{
+//     console.log(j)
+//     j++
+// }while(j<5)
+
+// //functions
+// function average(x,y)
+// {
+//     return x+y/2
+// }
+// console.log(Math.round(average(2,3)))
+
+// //arrow function
+// const hello =() =>{
+//     console.log("Hello")
+// } 
+
+// hello()
+
+//String
+// let a = "Hamm\"ad"
+// console.log(a.length)
+// console.log(a[2])
+// // template literal 
+// console.log(`${a} is my friend`)
+
+
+
+
+
+
+
+
+
