e83a1f491d913c5328b94d2e44b45908fa89e129
Hammad-Zahoor <141130337+Hammad-Zahoor@users.noreply.github.com>
2024-03-21 00:18:56 +0500

    Update core_concepts.js

--- core_concepts.js
+++ core_concepts.js
@@ -577,4 +577,12 @@
 //     console.log("Yes executed")
 // }
 
-
+// let p = fetch("https://goweather.herokuapp.com/weather/Ny")
+// p.then((value)=>{
+//     console.log(value.status)
+//     console.log(value.ok)
+//     console.log(value.headers)
+//     return value.json()
+// }).then((value)=>{
+//     console.log(value)
+// })
